874cffdcb8e97f74d2edab79738f2915b64980cb
TonyNguyen <truong.nn@amanotes.com>
2022-05-10 16:47:14 +0700

    remove

--- src/App.js
+++ src/App.js
@@ -1,24 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import csvToJson from "./helpers/csvToJson";
 
 function App() {
+  const [csvData, setCsvData] = React.useState(null);
+
+  async function handleChangeFile(e) {
+    const file = e.target.files[0];
+    const csvData = await csvToJson(file);
+    const headers = Object.keys(csvData[0]);
+    const object = {
+      headers,
+      data: csvData,
+    };
+    setCsvData(object);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="mx-auto">
+        <h1 className="display-5 text-center">Upload File</h1>
+      </div>
+
+      <div className="container">
+        <input
+          accept=".csv"
+          type="file"
+          name="imageUpload"
+          id="inputFile"
+          className="hide"
+          onChange={handleChangeFile}
+        />
+        <label htmlFor="inputFile" className="dropzone">
+          Click to select csv files
+        </label>
+
+        <br />
+
+        {csvData ? (
+          <>
+            <table className="table">
+              <thead className="thead-dark">
+                <tr>
+                  {csvData.headers.map((header, headerIndex) => (
+                    <th key={headerIndex} scope="col">
+                      {header}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+                {csvData.data.map((row, rowIndex) => {
+                  console.log("row", row);
+                  return (
+                    <tr key={rowIndex}>
+                      {Object.values(row).map((cell, cellIndex) => (
+                        <td key={cellIndex}>{cell}</td>
+                      ))}
+                    </tr>
+                  );
+                })}
+              </tbody>
+            </table>
+          </>
+        ) : (
+          <div className="text-center">no data</div>
+        )}
+      </div>
+    </>
   );
 }
 
